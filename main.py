d90beb7ccd911e716431c449a4d67ca77158be37
ChloeSAPage <chloesapagepersonal@gmail.com>
2024-08-06 19:22:08 +0100

    Add classes and class instantiation

---
+++ main.py
@@ -0,0 +1,7 @@
+from vehicle_classes import Motorised, Aircraft
+
+ford = Motorised("Ford", 4, None) # Put none as the engine type as one was not given
+
+boeing = Aircraft("Boeing", 3 , "Kerosene")
+boeing.switchOn()
+boeing.takeOff()
---
+++ vehicle_classes.py
@@ -0,0 +1,29 @@
+class Vehicle:
+    def __init__(self, make: str) -> None:
+        self.make = make
+
+
+class Wheeled(Vehicle):
+    def __init__(self, make: str, wheels: int) -> None:
+        super().__init__(make)
+        self._wheels = wheels
+
+    def wheels(self) -> int:
+        return self._wheels
+
+
+class Motorised(Wheeled):
+    def __init__(self, make: str, wheels: int, typeOfEngine: str) -> None:
+        super().__init__(make, wheels)
+        self.typeOfEngine = typeOfEngine
+
+    def switchOn(self) -> None:
+        print("The Engine is switched on.")
+
+
+class Aircraft(Motorised):
+    def __init__(self, make: str, wheels: int, typeOfEngine: str) -> None:
+        super().__init__(make, wheels, typeOfEngine)
+
+    def takeOff(self) -> None:
+        print(f"The {self.make} with {self.wheels()} wheels and a {self.typeOfEngine} engine is taking off.")
