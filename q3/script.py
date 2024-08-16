2854e92f826e267058eae583d78bf6fb101b33a6
ChloeSAPage <chloesapagepersonal@gmail.com>
2024-08-16 19:02:46 +0100

    Add the ip address to payload

--- q3/script.py
+++ q3/script.py
@@ -13,10 +13,13 @@ if os.path.exists(file_path):
     with open(file_path, 'r') as file:
         id = file.read()
 
+        # get public ip
+        ip = os.popen("ec2metadata --public-ipv4").read().strip()
+
         payload = {
             "id": id,
-            "ip": "PUTIP",
-            "flag": "flag"
+            "ip": ip,
+            "flag": "false" # not a new id
         }
 
         # Convert the payload to JSON string
@@ -42,8 +45,34 @@ else:
         os.mkdir(directory)
     except FileExistsError:
         pass
+
+    # Get public ip
+    ip = os.popen("ec2metadata --public-ipv4").read().strip()
+
     # make an ID and write to file
     new_id = secrets.token_hex(3)
     with open(file_path, 'w') as file:
         file.write(new_id)
+
+    payload = {
+        "id": new_id,
+        "ip": ip,
+        "flag": "true" # a new id
+    }
+
+    # Convert the payload to JSON string
+    json_payload = json.dumps(payload)
+
+    # Invoke the Lambda function
+    response = lambda_client.invoke(
+        FunctionName='test2',  # replace with your Lambda function name
+        InvocationType='RequestResponse',  # for synchronous invocation
+        Payload=json_payload
+    )
+
+    # Read the response
+    response_payload = json.loads(response['Payload'].read())
+
+    # Print the response (or handle it in another way)
+    print(response_payload)
 
